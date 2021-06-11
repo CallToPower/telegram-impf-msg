56d3aa6ee95d8e8d5d2f0c091e35d1504bd223ac
Denis Meyer <denis.meyer@protonmail.com>
2021-06-11 11:16:49 +0200

    Fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ if [[ ! -e /tmp/telegram-impf-msg.py.pid ]]; then
     echo "Starting telegram-impf-msg..."
     # python3 raspi-sapis.py <username>:<password> &
     source venv/bin/activate
-    python Main.py &
+    python3 Main.py &
     echo $! > /tmp/telegram-impf-msg.py.pid
     echo "telegram-impf-msg has been started with pid "
     cat /tmp/telegram-impf-msg.py.pid
