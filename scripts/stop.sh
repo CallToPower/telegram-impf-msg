a1d5de55d2e8754f08b899ebc5e02de6968da6dd
Denis Meyer <denis.meyer@protonmail.com>
2021-06-11 11:33:03 +0200

    Fix

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 if [[ -e /tmp/telegram-impf-msg.py.pid ]]; then
     echo "telegram-impf-msg is running, stopping..."
-    kill `cat /tmp/raspi-sapis.py.pid`
+    kill `cat /tmp/telegram-impf-msg.py.pid`
     rm /tmp/telegram-impf-msg.py.pid
     echo "telegram-impf-msg has been stopped"
 else
